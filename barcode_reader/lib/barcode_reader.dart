8e2c6bf14f4b8ed765584d409721c5be7d8eff18
ahmetenes <ahmetenessemerci@hotmail.com>
2020-07-09 21:05:35 +0300

    Added invoke method for Barcode detection

--- barcode_reader/lib/barcode_reader.dart
+++ barcode_reader/lib/barcode_reader.dart
@@ -3,16 +3,23 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class BarcodeReader {
-  static const MethodChannel _channel =
-      const MethodChannel('Reader');
+  static const MethodChannel _channel = const MethodChannel('Reader');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
+
   static Future<int> get openCamera async {
-    final Map<dynamic,dynamic> resultMap = await _channel.invokeMethod('openCamera');
+    final Map<dynamic, dynamic> resultMap =
+        await _channel.invokeMethod('openCamera');
 
     return resultMap["texture_id"];
+  }
+
+  static Future<String> get detectBarcode async {
+    final String barcode = await _channel.invokeMethod('detectBarcode');
+
+    return barcode;
   }
 }
