03826ddf0737d9ba12b958c286651e9d30ff82ed
ahmetenes <ahmetenessemerci@hotmail.com>
2020-07-09 05:09:11 +0300

    Changed the name of the MethodChannel from the name which was same with plugins name. "openCamera" method added.

--- barcode_reader/lib/barcode_reader.dart
+++ barcode_reader/lib/barcode_reader.dart
@@ -4,10 +4,15 @@ import 'package:flutter/services.dart';
 
 class BarcodeReader {
   static const MethodChannel _channel =
-      const MethodChannel('barcode_reader');
+      const MethodChannel('Reader');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+  static Future<int> get openCamera async {
+    final Map<dynamic,dynamic> resultMap = await _channel.invokeMethod('openCamera');
+
+    return resultMap["texture_id"];
   }
 }
