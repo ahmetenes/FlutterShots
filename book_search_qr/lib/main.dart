0aa5a6bc6daa5d89522e18f5afe5e4267b402b0a
ahmetenes <ahmetenessemerci@hotmail.com>
2020-07-10 23:55:27 +0300

    Implemented Barcode Search Screen

--- book_search_qr/lib/main.dart
+++ book_search_qr/lib/main.dart
@@ -1,117 +1,303 @@
+import 'dart:collection';
+import 'dart:ui';
+
+import 'package:book_search_qr/src/all_books.dart';
+import 'package:book_search_qr/src/search_result_bloc.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:barcode_reader/barcode_reader.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(BookSearchApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class BookSearchApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: Colors.brown,
+        backgroundColor: Colors.brown,
+        scaffoldBackgroundColor: Colors.brown,
+        accentColor: Colors.white,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "QRBook",
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  var _currentPage = 1;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QRBook"),
+        leading: Icon(Icons.bookmark),
+      ),
+      body: _currentPage == 1 ? SearchScreen() : SearchBarcodeScreen(),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedIconTheme: IconThemeData(
+          color: Colors.blue,
+        ),
+        currentIndex: _currentPage,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_books),
+            title: Text("Library"),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search), title: Text("Search")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.camera), title: Text("Barcode"))
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentPage = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class SearchBarcodeScreen extends StatefulWidget {
+  @override
+  _SearchBarcodeScreenState createState() => _SearchBarcodeScreenState();
+}
+
+class _SearchBarcodeScreenState extends State<SearchBarcodeScreen> {
+  final BarcodeReader reader = new BarcodeReader();
+  int textureId;
+  Future<void> getTextureId() async {
+    int _textureId;
+    _textureId = await BarcodeReader.openCamera;
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      textureId = _textureId;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getTextureId();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        textureId != null
+            ? Flexible(flex: 1, child: Texture(textureId: textureId))
+            : Center(
+
+              child: CircularProgressIndicator(
+                  value: null,
+                ),
+            ),
+      ],
+    );
+  }
+}
+
+class SearchScreen extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  String searchTerm = "İlk";
+  final bloc = SearchResultBloc();
+
+  _updateSearch(String str) {
+    setState(() {
+      searchTerm = str;
+      this.bloc.updateSearch(str);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Column(
+      children: <Widget>[
+        MySearchBar(
+          parentAction: _updateSearch,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Expanded(
+            child: SearchResult(
+          bloc: bloc,
+        ))
+      ],
     );
+  }
+}
+
+class SearchResult extends StatefulWidget {
+  final SearchResultBloc bloc;
+
+  SearchResult({Key key, this.bloc}) : super(key: key);
+
+  @override
+  _SearchResultState createState() => _SearchResultState();
+}
+
+class _SearchResultState extends State<SearchResult> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<UnmodifiableListView<Book>>(
+      stream: widget.bloc.books,
+      initialData: UnmodifiableListView<Book>([]),
+      builder: (context, snapshot) {
+        return ListView(
+          children: snapshot.data.isNotEmpty
+              ? snapshot.data.map(_buildBookItem).toList()
+              : [
+                  ListTile(
+                    leading: CircularProgressIndicator(value: null,),
+                    title: Text("Loading"),
+                  )
+                ],
+        );
+      },
+    );
+  }
+}
+
+Widget _buildBookItem(Book b) {
+  return Padding(
+    key: Key(b.id.toString()),
+    padding: const EdgeInsets.all(8.0),
+    child: new Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+        color: Colors.grey[300],
+      ),
+      padding: EdgeInsets.all(10.0),
+      child: new ExpansionTile(
+        leading: Icon(Icons.bookmark),
+        title: Text(
+          b.volumeInfo.title,
+          style: TextStyle(
+            fontSize: 16.0,
+          ),
+        ),
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              b.volumeInfo.industryIdentifiers.length > 0
+                  ? Text(b.volumeInfo.industryIdentifiers.first.identifier
+                      .toString())
+                  : Text("No ISBN"),
+              RawMaterialButton(
+                highlightColor: Colors.transparent,
+                fillColor: Colors.transparent,
+                child: IconButton(
+                  splashColor: Colors.blue,
+                  iconSize: 30,
+                  icon: Icon(Icons.link),
+                  onPressed: () {
+                    launch(b.selfLink);
+                  },
+                ),
+                padding: EdgeInsets.all(15.0),
+                shape: CircleBorder(),
+              )
+            ],
+          ),
+        ],
+        initiallyExpanded: false,
+      ),
+    ),
+  );
+}
+
+class MySearchBar extends StatefulWidget {
+  final void Function(String value) parentAction;
+
+  MySearchBar({Key key, this.parentAction}) : super(key: key);
+
+  @override
+  _MySearchBarState createState() => _MySearchBarState();
+}
+
+class _MySearchBarState extends State<MySearchBar> {
+  final isVertical = false;
+  final _controller = TextEditingController();
+  _SearchScreenState parentState;
+
+  String searchTerm;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      final text = _controller.text;
+      _controller.value = _controller.value.copyWith(
+          text: text,
+          selection:
+              TextSelection(baseOffset: text.length, extentOffset: text.length),
+          composing: TextRange.empty);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.grey[300],
+      ),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: "Search",
+              ),
+              onTap: () {},
+            ),
+          ),
+          RawMaterialButton(
+            highlightColor: Colors.transparent,
+            fillColor: Colors.transparent,
+            child: IconButton(
+              splashColor: Colors.blue,
+              iconSize: 30,
+              icon: Icon(Icons.search),
+              onPressed: () {
+                widget.parentAction(_controller.value.text);
+                _controller.clear();
+              },
+            ),
+            padding: EdgeInsets.all(15.0),
+            shape: CircleBorder(),
+          )
+        ],
+      ),
+    );
+  }
+
+  void getInput(String searchTerm) {
+    print(searchTerm);
   }
 }
