d86c0efb19a5e43afd3ee3ff69d1a3ada3988bd1
ahmetenes <ahmetenessemerci@hotmail.com>
2020-07-10 23:55:43 +0300

    Created bloc element

---
+++ book_search_qr/lib/src/search_result_bloc.dart
@@ -0,0 +1,50 @@
+import 'dart:collection';
+
+import 'package:book_search_qr/src/all_books.dart';
+import 'package:http/http.dart' as http;
+import 'package:rxdart/rxdart.dart';
+
+class SearchResultBloc {
+  final _booksSubject = BehaviorSubject<UnmodifiableListView<Book>>();
+
+  Stream<UnmodifiableListView<Book>> get books => _booksSubject.stream;
+
+  var _books = <Book>[];
+  String _searchTerm = "faust";
+  SearchResultBloc() {
+    _getBooks().then((_) {
+      _booksSubject.add(UnmodifiableListView(_books));
+    });
+  }
+  Future<Null> updateSearch(String str) async {
+    _books.clear();
+    _searchTerm = str;
+
+    return _getBooks().then((_) {
+      _booksSubject.add(UnmodifiableListView(_books));
+    });
+  }
+
+  Future<Null> _getBooks() async {
+    List<Book> _booklist = await getBookItems(_searchTerm);
+
+    _books = _booklist;
+  }
+
+  Future<List<Book>> getBookItems(String name) async {
+    final url =
+        "https://www.googleapis.com/books/v1/volumes?q=$_searchTerm &fields=items";
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      return parseBookList(response.body.toString());
+    } else {
+      throw NothingFetchedError('Anything found');
+    }
+  }
+}
+
+class NothingFetchedError {
+  final String errMsg;
+
+  NothingFetchedError(this.errMsg);
+}
